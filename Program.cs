d23b8942e54beff269004c2f3cb4f11dbdc65709
Emil Hytting <emilmm21@gmail.com>
2024-04-24 14:45:28 +0200

    D1
    Lavet en ny fil kaldet Currency.cs - Filen indeholder en enum. Hvor de forskellige valutaere er i.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using LNE_ERP;
+
 Console.WriteLine("Hello, World!");
+
+Currency valuta = Currency.EUR;
+if ( valuta == Currency.EUR)
+{
+    Console.WriteLine(100 * 7.43);
+}
