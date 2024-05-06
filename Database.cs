4f7ea0274c5425bc9ef9ec52cc0a9fd56243d6e2
Jakob-Hjort <jakob.hjort@gmail.com>
2024-05-06 10:45:07 +0200

    lavet en customerlist liste

--- Database.cs
+++ Database.cs
@@ -12,6 +12,7 @@ namespace LNE_ERP
         public static Database instance { get; set; } = new Database();
         List<Company> companyList = new List<Company>();
         List<SalesOrder> salesorderlist = new List<SalesOrder>();
+        List<Customer> customerlist = new List<Customer>();
    
 
     }
