ad0a8e6e5658cb4f49559da3c920d485a438b1ea
Jakob-Hjort <jakob.hjort@gmail.com>
2024-05-08 10:17:07 +0200

    ændret i customer så nu virker addresse

--- Customer/Customer.cs
+++ Customer/Customer.cs
@@ -10,15 +10,43 @@ namespace LNE_ERP
     {
         public int CustomerNumber { get; set; }
         public DateTime LastPurchaseDate { get; set; }
-
-
-       /* public Customer(string firstname, string lastname, string address, string contactinfo, int customernumber, DateTime lastpurchasedate, string email, int phonenumber)
-         : base(firstname, lastname, address, contactinfo, email, phonenumber)
+        public string Street
         {
-            CustomerNumber = customernumber;
-            LastPurchaseDate = lastpurchasedate;
+            get
+            {
+                return Addresses.Streetname;
+            }
+            set
+            {
+                Addresses.Streetname = value;
+            }
+        }
 
-        }*/
+        public string Housenumer
+        {
+            get => Addresses.Housenumber;
+            set => Addresses.Housenumber = value;
+        }
+
+        public int postalcode
+        {
+            get => Addresses.Postalcode;
+            set => Addresses.Postalcode = value;
+        }
+
+        public string city
+        {
+            get => Addresses.City;
+            set => Addresses.City = value;
+        }
+
+        /* public Customer(string firstname, string lastname, string address, string contactinfo, int customernumber, DateTime lastpurchasedate, string email, int phonenumber)
+          : base(firstname, lastname, address, contactinfo, email, phonenumber)
+         {
+             CustomerNumber = customernumber;
+             LastPurchaseDate = lastpurchasedate;
+
+         }*/
     }
 }
 
