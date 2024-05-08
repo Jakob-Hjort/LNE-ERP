a39626c03e057b7a1a1ad44ab13f61ff61149804
TheCodingLeaf <luckygpmaker@gmail.com>
2024-05-08 10:02:19 +0200

    D7(DONE)

--- UI/MainMenu.cs
+++ UI/MainMenu.cs
@@ -19,7 +19,7 @@ namespace LNE_ERP
 
                 Menu menu = new();
                 menu.Add(new CompanyListPage());
-                //menu.Add(new SalesOrderListPage());
+                menu.Add(new SalesOrderListPage());
                 menu.Add(new CustomerListPage());
                 menu.Add(new ProductsListPage());
                 menu.Start(this);
